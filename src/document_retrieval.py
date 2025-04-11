91ba549921177bd3ef1fc7543dbe4931df346bee
Tim Hannifan <hannifan@gmail.com>
2025-04-11 14:15:08 -0500

    adding doc_retrieval

---
+++ src/document_retrieval.py
@@ -0,0 +1,10 @@
+
+
+class DocumentRetrieval:
+    def __init__(self, api_key):
+        """
+        Initialize the DocumentRetrieval class with an API key.
+        
+        :param api_key: The API key for authentication.
+        """
+        self.api_key = api_key
